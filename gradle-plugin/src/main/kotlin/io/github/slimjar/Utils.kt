0fd57b269c082a2ca6c74b1f415c2ffcc52cc5e8
DaRacci <racci@racci.dev>
2022-12-27 02:12:11 +1100

    style: remove star import

--- gradle-plugin/src/main/kotlin/io/github/slimjar/Utils.kt
+++ gradle-plugin/src/main/kotlin/io/github/slimjar/Utils.kt
@@ -3,7 +3,7 @@ package io.github.slimjar // ktlint-disable filename
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.tasks.TaskContainer
-import org.gradle.kotlin.dsl.* // ktlint-disable no-wildcard-imports
+import org.gradle.kotlin.dsl.getByType
 
 internal val TaskContainer.targetedJarTask: Task get() {
     return findByName("reobfJar")
